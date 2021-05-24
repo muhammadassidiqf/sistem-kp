05ace7f64f6eff7578d7ada0506edfe2c669b48a
massidiqfattah <massidiqfattah@gmail.com>
2021-05-24 22:59:47 +0700

    host

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://kp-itenas.online/';
+$config['base_url'] = 'http://kp-itenas.online/';
 date_default_timezone_set('Asia/Jakarta');
 /*
 |--------------------------------------------------------------------------
